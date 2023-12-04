72c62a80e40dd25b934e52121747294734c0a9d0
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2023-12-04 22:25:50 +0500

    fix: change burger menu

--- coffee-house/js/app.js
+++ coffee-house/js/app.js
@@ -1,6 +1,25 @@
-document.querySelector('.header__burger').addEventListener('click', function() {
-  this.classList.toggle('header__burger--active');
-  document.querySelector('.header').classList.toggle('open__header');
-  document.querySelector('.header__nav').classList.toggle('open');
-  document.querySelector('.nav__list').classList.toggle('open');
-})
+const headerEl = document.querySelector('.header');
+const headerNavEl = document.querySelector('.header__nav');
+const navListEl = document.querySelector('.nav__list');
+const headerBurgerEl = document.querySelector('.header__burger');
+
+function toggleBurgerButton() {
+  document.querySelector('.header__burger').addEventListener('click', function() {
+    this.classList.toggle('header__burger--active');
+    headerEl.classList.toggle('open__header');
+    headerNavEl.classList.toggle('header__nav--active');
+    navListEl.classList.toggle('nav__list--active');
+  })
+}
+
+function closeBurgerMenu() {
+  document.querySelector('.header__nav').addEventListener('click', () => {
+    headerEl.classList.contains('open__header') ? headerEl.classList.remove('open__header') : false ;
+    headerNavEl.classList.contains('open') ? headerNavEl.classList.remove('header__nav--active') : false ;
+    navListEl.classList.contains('nav__list--active') ? navListEl.classList.remove('nav__list--active') : false ;
+    headerBurgerEl.classList.contains('header__burger--active') ? headerBurgerEl.classList.remove('header__burger--active') : false ;
+  })
+}
+
+toggleBurgerButton();
+closeBurgerMenu();
