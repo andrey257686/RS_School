22eb860d3bcb1790bd29aacdac45cadc8cdfa72c
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-01-12 23:18:39 +0500

    feat: dynamically load game field

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
+const word = "солнце";
+const hint = "Звезда, освещающая Землю и обеспечивающая тепло.";
+const container = document.querySelector(".container");
+const mapSVG = new Map([
+  [1, "head.svg"],
+  [2, "body.svg"],
+  [3, "hand-one.svg"],
+  [4, "hand-two.svg"],
+  [5, "leg-one.svg"],
+  [6, "leg-two.svg"],
+]);
+
+const createFieldWord = function () {
+  const fieldWordElem = document.createElement("ul");
+  fieldWordElem.className = "field__word";
+  for (let i = 0; i < word.length; i++) {
+    const wordLetter = document.createElement("li");
+    wordLetter.className = "field__word_letter";
+    fieldWordElem.appendChild(wordLetter);
+  }
+  return fieldWordElem;
+};
+
+const checkLetter = function (buttonElem, letter) {
+  const listLetterElem = document.querySelectorAll(".field__word_letter");
+  if (word.toUpperCase().includes(letter)) {
+    buttonElem.disabled = true;
+    let index = word.toUpperCase().indexOf(letter);
+    listLetterElem[index].innerText = letter;
+  } else {
+    wrongTry = wrongTry + 1;
+    fieldGuessElem.innerHTML = `Неправильные попытки: <b>${wrongTry} / 6</b>`;
+    console.log(mapSVG.get(wrongTry));
+    gallowImg.src = `./img/${mapSVG.get(wrongTry)}`;
+  }
+};
+
 const createKeyboard = function () {
   let letters = [];
   for (let i = 1040; i <= 1071; i++) {
@@ -17,6 +54,9 @@ const createKeyboard = function () {
     const buttonKey = document.createElement("button");
     buttonKey.innerText = letters[i];
     buttonKey.className = "field__keyboard_key";
+    buttonKey.addEventListener("click", function (event) {
+      checkLetter(event.target, letters[i]);
+    });
 
     if (i >= 0 && i < 12) {
       keyboardLine1.appendChild(buttonKey);
@@ -35,6 +75,24 @@ const createKeyboard = function () {
   return keyboardField;
 };
 
-const field = document.querySelector(".field");
+const createField = function () {
+  const fieldElem = document.createElement("section");
+  fieldElem.className = "field";
+  fieldElem.appendChild(createFieldWord());
+  const fieldHintElem = document.createElement("p");
+  fieldHintElem.className = "field__hint";
+  fieldHintElem.innerHTML = `Подсказка: <b>${hint}</b>`;
+  fieldElem.appendChild(fieldHintElem);
+  const fieldGuessElem = document.createElement("p");
+  fieldGuessElem.className = "field__guess";
+  fieldGuessElem.innerHTML = `Неправильные попытки: <b>0 / ${word.length}</b>`;
+  fieldElem.appendChild(fieldGuessElem);
+  fieldElem.appendChild(createKeyboard());
+  return fieldElem;
+};
 
-field.appendChild(createKeyboard());
+container.appendChild(createField());
+
+let wrongTry = 0;
+const fieldGuessElem = document.querySelector(".field__guess");
+const gallowImg = document.querySelector(".gallows__img");
