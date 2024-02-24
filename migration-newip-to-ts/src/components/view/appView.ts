545185bdf5a9ba67128679363f072889c46713cc
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 17:09:00 +0500

    feat: implement types

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { IArticleData, ISourceData } from '../../types';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: IArticleData) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: ISourceData) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
