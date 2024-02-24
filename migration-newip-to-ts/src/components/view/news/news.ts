8f365fd866174cd9ffe8bdba7c095ee4280601f7
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 17:12:05 +0500

    fix: add type

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -3,7 +3,7 @@ import { IArticle } from '../../../types';
 
 class News {
     draw(data: IArticle[]) {
-        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+        const news: IArticle[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment() as DocumentFragment;
         const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
