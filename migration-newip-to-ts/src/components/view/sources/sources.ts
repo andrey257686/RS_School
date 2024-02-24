4373a4e8453c8dc9c67eea55e50324851577de11
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 16:51:52 +0500

    feat: add types

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import { ISource } from '../../../types';
+
+class Sources {
+    draw(data: ISource[]) {
+        const fragment = document.createDocumentFragment() as DocumentFragment;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLDivElement;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLSpanElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLDivElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLDivElement).append(fragment);
+    }
+}
+
+export default Sources;
