4e32dd73463d1121e907cd551d3de1d59da5e2b8
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 15:41:18 +0500

    feat: implement types

---
+++ migration-newip-to-ts/src/types.ts
@@ -0,0 +1,40 @@
+export interface ISource {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
+
+export interface ISourceData {
+  status: string;
+  sources: ISource[];
+}
+
+export interface IArticle {
+  source: {
+    id: string;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+export interface IArticleData {
+  status: string;
+  totalResults: number;
+  articles: IArticle[];
+}
+
+export interface IError {
+  status: string;
+  code: string;
+  message: string;
+}
