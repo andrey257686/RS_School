3c8c93fff8774755ed62fac5180ad260b711b179
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 20:21:00 +0500

    feat: implement types

--- migration-newip-to-ts/src/components/controller/controller.ts
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
 import AppLoader from './appLoader';
+import { ISourceData } from '../../types';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (data: ISourceData) => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +11,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews<T>(e: Event, callback: (data: T) => void) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLDivElement;
         }
     }
 }
