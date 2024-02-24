61d1b4d029d1832b35647ab2d6f0cfad5c37ef17
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-24 19:57:41 +0500

    feat: implement types

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+        super(process.env.API_URL as string, {
+            apiKey: process.env.API_KEY as string,
         });
     }
 }
