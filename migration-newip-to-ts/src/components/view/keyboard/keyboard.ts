09c557fabc97f70b10cd980b6686726162bfb187
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-02-26 18:53:02 +0500

    fix: add types

--- migration-newip-to-ts/src/components/view/keyboard/keyboard.ts
+++ migration-newip-to-ts/src/components/view/keyboard/keyboard.ts
@@ -6,22 +6,19 @@ class Keyboard {
         for (let i = 0; i < 26; i++) {
             letters.push(String.fromCharCode(i + 65));
         }
-        const keyboardField = document.querySelector('.keyboard') as HTMLDivElement;
+        const keyboardField: HTMLDivElement = document.querySelector('.keyboard') as HTMLDivElement;
         keyboardField.innerHTML = '';
-        const buttonKey = document.createElement('button');
+        const buttonKey: HTMLButtonElement = document.createElement('button') as HTMLButtonElement;
         buttonKey.className = 'keyboard__key';
         buttonKey.id = 'key_all';
         buttonKey.innerText = 'ALL';
         keyboardField.appendChild(buttonKey);
 
         for (let i = 0; i < letters.length; i++) {
-            const buttonKey = document.createElement('button');
+            const buttonKey: HTMLButtonElement = document.createElement('button') as HTMLButtonElement;
             buttonKey.innerText = letters[i];
             buttonKey.className = 'keyboard__key';
             buttonKey.id = `key_${i}`;
-            // buttonKey.addEventListener('click', function (event) {
-            //     event.preventDefault();
-            // });
             keyboardField.appendChild(buttonKey);
         }
     }
