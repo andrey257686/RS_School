33043b9f3ad5ed591083bf3db0081b6e39799919
Andrey <94921563+andrey257686@users.noreply.github.com>
2024-03-13 17:23:42 +0500

    feat: implement vite for using in docker (#39)

--- rss-puzzle/vite.config.ts
+++ rss-puzzle/vite.config.ts
@@ -4,6 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: './',
   plugins: [tsconfigPaths()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173
+  },
   css: {
     modules: {
       localsConvention: 'camelCase',
