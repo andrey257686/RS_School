70ebe63b9191359392fde2929afa462d5652d2cb
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-03-25 19:45:50 +0500

    init: start async-race task

---
+++ async-race/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint-config-airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["prettier", "import", "@typescript-eslint"],
+  rules: {},
+  noInlineConfig: true,
+};
