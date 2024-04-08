d6fd50f2ee48ed65d9d1d4b6090a255560d7b5b1
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-04-08 23:05:32 +0500

    feat: implement initial configuration

---
+++ fun-chat/.eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint-config-airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["import", "eslint-plugin-import", "@typescript-eslint", "prettier"],
+  rules: {
+    "no-console": 0,
+  },
+};
