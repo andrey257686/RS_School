91f852ddef5a81f69ef1c58781051ac9985b3f50
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-06-25 22:23:04 +0500

    fix: fix bug

--- coffee-house/js/app.js
+++ coffee-house/js/app.js
@@ -1,33 +1,35 @@
 // ============ Реализация Бургера ============
 
-const headerEl = document.querySelector('.header');
-const headerNavEl = document.querySelector('.header__nav');
-const navListEl = document.querySelector('.nav__list');
-const headerBurgerEl = document.querySelector('.header__burger');
+const headerEl = document.querySelector(".header");
+const headerNavEl = document.querySelector(".header__nav");
+const navListEl = document.querySelector(".nav__list");
+const headerBurgerEl = document.querySelector(".header__burger");
 let burgerVisible = false;
 
 function toggleBurgerButton() {
-  document.querySelector('.header__burger').addEventListener('click', function() {
+  document.querySelector(".header__burger").addEventListener("click", function () {
     burgerVisible = burgerVisible === false ? true : false;
-    document.body.style.overflow = burgerVisible === false ? '' : 'hidden'
-    console.log(burgerVisible)
-    this.classList.toggle('header__burger--active');
-    // headerEl.classList.toggle('open__header');
-    headerNavEl.classList.toggle('header__nav--active');
-    navListEl.classList.toggle('nav__list--active');
-  })
+    // document.body.style.overflow = burgerVisible === false ? "" : "hidden";
+    this.classList.toggle("header__burger--active");
+    headerNavEl.classList.toggle("header__nav--active");
+    navListEl.classList.toggle("nav__list--active");
+  });
 }
 
 function closeBurgerMenu() {
-  document.querySelector('.header__nav').addEventListener('click', () => {
-    // headerEl.classList.contains('open__header') ? headerEl.classList.remove('open__header') : false ;
+  document.querySelector(".header__nav").addEventListener("click", () => {
     burgerVisible = burgerVisible === false ? true : false;
-    document.body.style.overflow = burgerVisible === false ? '' : 'hidden'
-    console.log(burgerVisible)
-    headerNavEl.classList.contains('open') ? headerNavEl.classList.remove('header__nav--active') : false ;
-    navListEl.classList.contains('nav__list--active') ? navListEl.classList.remove('nav__list--active') : false ;
-    headerBurgerEl.classList.contains('header__burger--active') ? headerBurgerEl.classList.remove('header__burger--active') : false ;
-  })
+    // document.body.style.overflow = burgerVisible === false ? "" : "hidden";
+    headerNavEl.classList.contains("open")
+      ? headerNavEl.classList.remove("header__nav--active")
+      : false;
+    navListEl.classList.contains("nav__list--active")
+      ? navListEl.classList.remove("nav__list--active")
+      : false;
+    headerBurgerEl.classList.contains("header__burger--active")
+      ? headerBurgerEl.classList.remove("header__burger--active")
+      : false;
+  });
 }
 
 toggleBurgerButton();
